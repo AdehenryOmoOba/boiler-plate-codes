afe05b6a903e188b4f1d64737bf9a6ff8d66005e
Adefemi Adelaja <adefemiadelaja@gmail.com>
2022-08-09 10:01:21 +0100

    pagination

--- controllers/students.js
+++ controllers/students.js
@@ -2,13 +2,20 @@ import { studentsPool as pool } from "../postgresDB.js";
 
 export const getAllStudentsController = async (req, res, next) => {
   try {
+    const { rows: rowCount } = await pool.query(
+      "SELECT COUNT(*) FROM students"
+    );
+    const { count } = rowCount[0];
+    const pages = Math.ceil(Number(count) / 10);
     const { page } = req.query;
     const offset = !page || Number(page) === 1 ? 0 : Number(page) * 10 - 10;
     const { rows } = await pool.query(
       "SELECT * FROM students OFFSET $1 limit 10",
       [offset]
     );
-    res.status(200).json({ page: Number(page) || 1, students: rows });
+    res
+      .status(200)
+      .json({ page: `${Number(page || 1)} of ${pages}`, students: rows });
   } catch (error) {
     next(error);
   }
